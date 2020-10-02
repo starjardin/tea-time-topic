986f2a29b5882630ebd59b29dfd58cc4ecd09de0
Tantely Andrianarivola <tantely.and@onja.org>
2020-10-02 07:48:30 +0300

    fetched topics from the api

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+/*TODO : 
+  1- fetch the topics list, // 4- sort goes here
+  2- create function to display the lists, 
+  3- creat a function to add topis via form
+  4- listen for upvoted and downvoted
+*/
+
 const list = document.querySelector(".container");
 const formEl = document.querySelector("form");
+
+const endPoint = "https://gist.githubusercontent.com/Pinois/93afbc4a061352a0c70331ca4a16bb99/raw/6da767327041de13693181c2cb09459b0a3657a1/topics.json";
+
+let listTopics = [];
+
+async function fecthTopics () {
+  const res = await fetch(`${endPoint}`);
+  const data = await res.json();
+  listTopics.push(...data);
+};
+
+function displayListTopics () {
+
+}
